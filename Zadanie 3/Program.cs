7f9be1b2578d71019b258a74c41cf7daad635a08
FleFod <poopfart121787@gmail.com>
2025-03-03 16:32:36 +0100

    Zakończone zadania 1-5

---
+++ Zadanie 3/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zadanie_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+            //Napisać program wczytujący z klawiatury liczbę całkowitą i wypisujący na ekranie sześcian
+        {
+            int a;
+            Console.WriteLine("Podaj a:");
+            a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("a ^ 3 ={0}", a * a * a);
+            Console.ReadKey(true);
+        }
+    }
+}
