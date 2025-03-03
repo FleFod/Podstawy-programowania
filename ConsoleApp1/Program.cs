db078407580913c9714ea47355ac44e7083ff2f7
wzr_student <wzr_student@WZR-C24-02>
2025-03-03 15:22:28 +0100

    Dodaj pliki projektów.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        /// <summary>
+        /// metoda startowa
+        /// </summary>
+        /// <param name="args">argumenty metody</param>
+        static void Main(string[] args)
+        {
+            Console.WriteLine("hamburger cheeseburger big mac whopper"); //waffentrager
+            Console.ReadKey(true);
+           /*
+           op4
+           located
+           the bomb
+           */
+        }
+    }
+}
