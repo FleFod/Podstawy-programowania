bbfd2411cd9aee05a0848d43622e86b8bca0a589
FleFod <poopfart121787@gmail.com>
2025-04-04 14:23:09 +0200

    zadania 1-5

---
+++ Pętle while 4/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pętle_while_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        /*Wczytaj dwie liczby naturalne dodatnie n i k. Narysuj z gwiazdek na ekranie prostokąt
+posiadający n wierszy i k kolumn. Użyj pętli while.*/
+        {
+            int n, k, i, j;
+            Console.WriteLine("Podaj liczbę wierszy ");
+            n = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Podaj liczbę kolumn ");
+            k = Convert.ToInt32(Console.ReadLine());
+            i = 1;
+            while (i <= n)
+            {
+                j = 1;
+                while (j <= n)
+                {
+                    Console.Write("*");
+                    j++;
+                }
+                Console.WriteLine("");
+                i++;
+            }
+            Console.ReadKey(false);
+        }
+    }
+}
---
+++ Pętle while 5/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pętle_while_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        /*Napisać program wczytujący od użytkownika liczbę rzeczywistą x. Wczytuj tak długo wartość
+zmiennej x, aż przyjmie ona wartość dodatnią. Wczytaną dodatnią wartość liczby x wypisz na
+ekranie. Użyj pętli do-while.*/
+        {
+            double x;
+            do
+            {
+                Console.WriteLine("Podaj Dodatnią Liczbę X ");
+                x = Convert.ToDouble(Console.ReadLine());
+            } while (x <= 0);
+            Console.WriteLine("Podano X = {0}", x);
+            Console.ReadKey(false);
+           
+        }
+    }
+}
